c4bca715bad2d6c05f3f31b501cdc0dcaa39c5ad
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-03-28 18:24:56 +0530

    Add files via upload

---
+++ Print character in range/Print character in range-G to R.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   char ch='\0';
+
+   for(ch='G';ch<='R';ch++)
+   {
+      printf("%c\n",ch);
+   }
+   printf("Thanks!!!\n");
+
+   getche ();
+   return 0;
+}
---
+++ Print character in range/print character in reverse range user specified.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   char ch='\0',S_char=0,E_char=0;
+
+   printf("Enter the range of characters:\n");
+
+   printf("Enter the starting character:\n");
+   S_char=getche();
+  
+   printf("\n");
+
+   printf("Enter the ending character:\n");
+   E_char=getche();
+  
+   printf("\n\n");
+
+   printf("The characters are:\n");
+   for(ch=S_char;ch>=E_char;ch--)
+   {
+      printf("%c\n",ch);
+   }
+   printf("Thanks!!!\n");
+
+   getche ();
+   return 0;
+}
