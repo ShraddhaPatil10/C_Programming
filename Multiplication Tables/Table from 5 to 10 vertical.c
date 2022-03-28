aa1906304dab57e741c6ecb728c9d6be78d033a2
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-03-28 20:07:10 +0530

    Add files via upload

---
+++ Multiplication Tables/Table from 5 to 10 vertical.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   int Num=0,cnt=1,S_Num=5,E_Num=10;
+
+   printf("*********Multiplication Table**********\n");
+
+   for(Num=S_Num;Num<=E_Num;Num++)for(cnt=1;cnt<=10;cnt++)
+   {
+     for(Num=S_Num;Num<=E_Num;Num++)
+     {
+       printf(" %2d ",(Num*cnt));
+     }
+      printf("\n");
+   }
+
+   printf("*********Multiplication Table**********\n");
+
+   printf("Thanks!!!\n");
+
+   getche();
+   return 0;
+}
