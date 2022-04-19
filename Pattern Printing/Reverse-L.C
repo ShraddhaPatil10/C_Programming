21a2390fe180fd3634501845e66815a36f691ab3
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-04-19 23:14:59 +0530

    Add files via upload

---
+++ Pattern Printing/Reverse-L.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   int i=0,j=0,R=0,C=0;
+   printf("Enter value of row:\n\n");
+   scanf("%d",&R);
+
+   printf("Enter value of column:\n\n");
+   scanf("%d",&C);
+
+   printf("Pattern=>\n\n");
+
+   for(i=1;i<=R;i++)
+   {
+      for(j=1;j<=C;j++)
+       {
+         if(j==C || i==R)
+         {
+            printf(" * ");
+         }
+         else 
+         {
+             printf("   ");
+         }
+       }
+          printf("\n");
+    }
+      printf("Thanks!!\n");
+      getch();
+      return 0;
+}  
+        
+        
+        
