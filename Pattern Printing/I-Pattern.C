291952ce77087909d5b39c18074529ce3d4966bc
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-04-20 20:33:46 +0530

    Add files via upload

---
+++ Pattern Printing/I-Pattern.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   int i=0,j=0,X=0;
+   printf("Enter value of row and column:\n\n");
+   scanf("%d",&X);
+
+
+   printf("Pattern=>\n\n");
+
+   for(i=1;i<=X;i++)
+   {
+      for(j=1;j<=X;j++)
+       {
+         if(i==1 || i==X || j==(X/2)+1)
+         {
+            printf(" * ");
+         }
+         else 
+         {
+             printf("   ");
+         }
+       }
+          printf("\n");
+    }
+      printf("Thanks!!\n");
+      getch();
+      return 0;
+}  
+        
+        
+        
