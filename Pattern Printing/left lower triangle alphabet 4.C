bd94860da7b899b266aeeaffd5758e09124adfd4
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-04-25 22:10:47 +0530

    Add files via upload

---
+++ Pattern Printing/left lower triangle alphabet 4.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    char ch='A';
+    int i=0,j=0,X=0;
+    printf("Enter Row and column value:\n");
+    scanf("%d",&X);
+    
+    printf("Pattern is=>\n");
+    
+    for(ch='A',i=1;i<=X;i++)
+    {
+       for(j=1;j<=X;j++)
+       {
+          if(i>=j)
+          {
+             printf(" %c ",ch);
+             
+           }
+           else
+           {
+              printf("   ");
+           }
+           ch++;
+        }
+        printf("\n");
+     }
+    
+    printf("Thanks!!!\n");
+    getch();
+    return 0;
+}
+        
