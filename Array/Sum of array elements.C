47e46a1f7fc7dadc715e5c6d01e3515e7f5645c7
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-05-01 15:18:45 +0530

    Add files via upload

---
+++ Array/Sum of array elements.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    int i=0, values[10]={},sum=0;
+    
+    printf("Enter array elements:\n");
+    
+    for(i=0;i<10;i++)
+    {
+       printf("\nEnter array elements %d:",i+1);
+       scanf("%d",& values[i]);
+       sum= sum + values[i];
+    }
+    printf("Press key to move ahead\n");
+    getch();
+    
+    printf("The addition of array elements=%d\n\n",sum);
+    printf("Elements in array are=>\n\n");
+    for(i=0;i<10;i++)
+    {
+       printf("Values of 101 element =%d\n",values[i]);
+    }
+    
+    getch();
+    return 0;
+}
+    
