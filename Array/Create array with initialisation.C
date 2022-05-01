6ea085a9b10e782bea8f11174affea16279d9afd
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-05-01 18:33:33 +0530

    Add files via upload

---
+++ Array/Create array with initialisation.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+   int values[5]={23,45,0,67,88};
+  
+   printf("Value of 1st element = %d\n", values[0]);
+   printf("Value of 2nd element = %d\n", values[1]);
+   printf("Value of 3rd element = %d\n", values[2]);
+   printf("Value of 4th element = %d\n", values[3]);
+   printf("Value of 5th element = %d\n", values[4]);
+
+   getch();
+   return 0;
+}
