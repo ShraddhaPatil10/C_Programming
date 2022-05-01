f2385177091e38ea0eb22939f76a86a012582f66
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-05-01 19:10:45 +0530

    Add files via upload

---
+++ Array/Create array and print count of-ve numbers.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define size 10
+
+int main ()
+{
+   int i=0,Bill[size]={0},cnt=0;
+
+   for(i=0;i<size;i++)
+   {
+       printf("Enter Bill no %d=",(i+1));
+       scanf("%d",&Bill[i]);
+
+       if(Bill[i]<0)
+      {
+          cnt++;
+      }
+   }
+     printf("The count of -ve numbers in array=>%d\n",cnt);
+    
+    getch();
+    return 0;
+}
+
+
