6f8314962ffb81425efbb1caa957f7406d80c85b
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-05-09 19:12:52 +0530

    Add files via upload

---
+++ Array/Create Array and display index of given element.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 5
+
+int main ()
+{
+    int i=0,Ele=0,Bill[size]={};
+    
+    for(i=0;i<size;i++)
+    {
+       printf("Enter Bill no %d=",(i+1));
+       scanf("%d",&Bill[i]);
+   }
+   getch();
+
+   printf("Enter Element to be searched in array=\n");
+   scanf("%d",&Ele);
+
+   for(i=0;i<size;i++)
+   {
+      if(Bill[i]==Ele)
+      {
+         break;
+      }
+  }
+
+  if(i==size)
+  {
+     printf("No such element found in array\n");
+  }
+  else
+  {
+     printf("Element %d found at Index %d",Ele,i);
+   }
+
+   getch();
+   return 0;
+}
