ac7d92c8cccc1baacbd76ebdf238197c123cf727
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-11 17:46:13 +0530

    Add files via upload

---
+++ Structure/Object of structure.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct student
+{
+    int Roll_No;
+    char SName[24];
+    float Per;
+    char course[16];
+};
+
+int main ()
+{
+    int Num;
+    
+    struct student std1;
+    
+    getch();
+    return 0;
+ }
+    
