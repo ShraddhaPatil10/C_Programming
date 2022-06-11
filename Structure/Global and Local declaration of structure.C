aaa95c5211b01af07e2cde485180f52547a6efd6
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-11 17:42:20 +0530

    Add files via upload

---
+++ Structure/Global and Local declaration of structure.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct student
+{
+    int Roll_No;
+    char SName[24];
+    float Per;
+    char course[16];
+};
+
+int main ()
+{
+    struct Employee
+    {
+        int EID;
+        char EName[24];
+        float Salary;
+        char Dept[20];
+        long int MobNo;
+    };
+    
+    getch();
+    return 0;
+ }
+    
