5957d8b8889d47e2d69124838e3fc575665a7fad
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-07-12 09:56:11 +0530

    Add files via upload

---
+++ Function/Create array without initialisation and display it using function.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Create_Array();
+
+int main()
+{
+    Create_Array();
+    getch();
+    return 0;
+}
+
+void Create_Array()
+{
+    int values[5];
+    
+    printf("\nThe value of 1st element = %d", values[0]);
+    printf("\nThe value of 2nd element = %d", values[1]);
+    printf("\nThe value of 3rd element = %d", values[2]);
+    printf("\nThe value of 4th element = %d", values[3]);
+    printf("\nThe value of 5th element = %d", values[4]);
+    
+    getch();
+    return;
+}
