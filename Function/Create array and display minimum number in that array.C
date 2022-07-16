11e1795690fd613101a9ce1823440c609f9ce14d
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-07-16 17:16:08 +0530

    Add files via upload

---
+++ Function/Create array and display minimum number in that array.C
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+#define size 5
+
+void Accept_Array(int Num[]);
+void Display_Array(int Arr[]);
+void Maximum(int Min_num[]);
+
+int main()
+{
+    int values[5]={0};
+    
+    Accept_Array(values);
+    
+    printf("\n\nBack to main()");
+    printf("\n\nPrss Any Key To Move Ahead.");
+    
+    getch();
+    
+    Display_Array(values);
+    
+    printf("\n\nPress key to move ahead.");
+    getch();
+    
+    Maximum(values);
+    
+    getch();
+    return 0;
+    
+}
+
+void Accept_Array(int Num[])
+{
+     int i=0;
+    
+     printf("\nInside Accept Element=>\n");
+    
+     for(i=0;i<size;i++)
+     {
+        printf("\nEnter Element No %d=",i+1);
+        scanf("%d",&Num[i]);
+     }
+      
+     return;
+}
+void Display_Array(int Arr[])
+{
+     int i=0;
+    
+      printf("\nElements In Array Inside Display Function");
+    
+      for(i=0;i<size;i++)
+      {
+          printf("\nValue of %d element = %d",i+1,Arr[i]);
+      }
+       
+      return;
+}
+
+void Maximum(int Min_num[])
+{
+    int i=0,min=0;
+    
+    printf("\n\nInside Maximum Function");
+    
+    for(i=0;i<size;i++)
+    {
+       if((i==0) || (Min_num[i]<min))
+       {
+           min=Min_num[i];
+       }
+    }
+     
+    printf("\nThe Minimum number is = %d",min);
+    
+    return;
+}
+    
+    
