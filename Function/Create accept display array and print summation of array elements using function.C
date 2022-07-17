2369b9f291b9ddc61bc35cb0a5ea15813a5cc34b
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-07-17 19:45:16 +0530

    Add files via upload

---
+++ Function/Create accept display array and print summation of array elements using function.C
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define size 5 
+
+void Accept_Array(int Num[]);
+void Display_Array(int Arr[]);
+int Summation(int Num[]);
+
+int main ()
+{
+    int cnt=0,values[5]={},Res=0;
+    
+    Accept_Array(values);
+    
+    printf("\nBack To Main()");
+    printf("\nPress key to move ahead.");
+    
+    getch();
+    
+    Display_Array(values);
+    
+    getch();
+     
+    Res=Summation(values);
+    
+    printf("\n\nSummation of array elements = %d\n",Res);       
+    getch();
+    return 0;
+}
+
+void Accept_Array(int Num[])
+{
+      int i=0;
+    
+      printf("\nInside Accept Array function=>\n");
+    
+      for(i=0;i<size;i++)
+      {
+         printf("\nEnter Element No %d =\n",i+1);
+         scanf("%d",&Num[i]);
+      }
+      return;
+}
+
+void Display_Array(int Arr[])
+{
+     int i=0;
+    
+      printf("\nElements In Array Inside Display Function");
+    
+      for(i=0;i<size;i++)
+      {
+          printf("\nValue of %d element = %d",i+1,Arr[i]);
+      }
+       
+      return;
+}
+
+int Summation(int Num[])
+{
+     int i=0,sum=0;
+    
+     for(i=0;i<size;i++)
+     {
+         sum=sum+Num[i];
+     }
+     
+     return sum;
+} 
